e747f08251f011df0a0638abd6b3f4ce062ed6dd
Maenzaga <maenzaga@gmail.com>
2021-10-09 23:56:05 +0200

    HTML layout and first sass implementation

--- booking-app/src/App.tsx
+++ booking-app/src/App.tsx
@@ -1,24 +1,47 @@
-import logo from "./logo.svg";
-import "./App.css";
+import "./App.scss";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+    <>
+      <header className="app-header">
+        <h2 className="app-title">Your Booking</h2>
+        <p className="app-subtitle">
+          If a booking is missing, ask your couple to mark your venue as booked
+          on the Bridebook app, and they will appear below automatically
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
-    </div>
+      <main className="main-content">
+        <div className="card-container">
+          {/* <div className="image-container" /> */}
+          <div className="booking-info">
+            <section className="booking-info__date-section">
+              <p className="booking-info__date-section--date">17 Oct, 2022</p>
+              <p className="booking-info__date-section--guests">70 guests</p>
+            </section>
+            <section className="booking-info__names-section">
+              <p className="booking-info__names-section--names">
+                Sarah & Chris
+              </p>
+              <p className="booking-info__names-section--mail">
+                sarah-chris@gmail.com
+              </p>
+            </section>
+          </div>
+          <div className="booking-confirmation">
+            <p className="booking-confirmation__connected">CONNECTED</p>
+            <section className="booking-confirmation__confirm-section">
+              <button
+                type="button"
+                className="booking-confirmation__confirm-section--button"
+              >
+                Confirm Booking
+              </button>
+              <p className="">Not my booking</p>
+            </section>
+          </div>
+        </div>
+      </main>
+    </>
   );
 }
 
